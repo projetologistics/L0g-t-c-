e15c125e0c2cc964cb6bb066b70483ab9ef5d6b0
Rodrigo Tadeu Alves de Moura <rodrigomoura.mva@outlook.com>
2017-04-04 14:09:55 -0300

    Domain

--- Logistics/Logistics.Infra.Data/Context/LogisticsContext.cs
+++ Logistics/Logistics.Infra.Data/Context/LogisticsContext.cs
@@ -2,6 +2,7 @@
 
 
 //using Logistics.Domain.Entities;
+using LogisticsDomain.Entities;
 using System.Data.Entity;
 
 namespace Logistics.Infra.Data.Context
@@ -13,7 +14,7 @@ namespace Logistics.Infra.Data.Context
             
         }
 
-      // public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
